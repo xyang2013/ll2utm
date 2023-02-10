7b4837151f33e0f56eb0994198bcd08df38a8424
Xiao Yang <xiao.yang17@gmail.com>
2023-02-11 10:56:21 +1100

    refactored code and added output data struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
+use std::env;
 use std::error::Error;
 use std::io;
-use std::env;
 
 use csv;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use utm;
-;
+
 #[derive(Debug, Deserialize)]
 struct SurveyPoint {
     point: u16,
@@ -15,33 +15,21 @@ struct SurveyPoint {
     ahd: f64,
 }
 
-/// Reads data from `stdin` into a reader and prints all records.
-/// 
-/// # Error
-/// 
-/// If an error occurs, the error is returned to `main`.
-fn read_from_stdin() -> Result<(), Box<dyn Error>> {
-    // Create a new csv Reader
-    let mut reader = csv::Reader::from_reader(io::stdin());
-    
-    // .records return an iterator of the internal
-    // record structure
-    for result in reader.records() {
-        let record = result?;
-
-        println!("{:?}", record);
-
-    }
-
-    Ok(())
+#[derive(Debug, Serialize)]
+struct PENZD {
+    point: u16,
+    easting: f64,
+    northing: f64,
+    level: f64,
+    description: String,
 }
 
 /// Reads data from a file into a reader and prints all records.
-/// 
+///
 /// # Error
-/// 
+///
 /// If an error occurs, the error is returned to `main`.
-fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
+fn read_from_file(path: &str) -> Result<Vec<PENZD>, Box<dyn Error>> {
     // Creates a new csv `Reader` from a file
     let mut reader = csv::Reader::from_path(path)?;
 
@@ -49,46 +37,44 @@ fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
     let headers = reader.headers()?;
     println!("{:?}", headers);
 
-    // // `.records` return an iterator of the internal
-    // // record structure
-    // for result in reader.records() {
-    //     let record = result?;
-
-    //     println!("{:?}", record);
-    // }
+    let mut penzd_points: Vec<PENZD> = vec![];
 
     // `.deserialize` returns an iterator of the internal
     // record structure deserialized
     for result in reader.deserialize() {
         let record: SurveyPoint = result?;
 
-        println!("{:?}", record);
-
         let (northing, easting, _) = utm::to_utm_wgs84_no_zone(record.latitude, record.longitude);
+        
+        let penzd = PENZD {
+            point: record.point,
+            easting: easting,
+            northing: northing,
+            level: record.ahd,
+            description: "".to_string(),
+        };
 
-        println!("{:?}", (northing, easting));
+        penzd_points.push(penzd);
+
     }
 
-    Ok(())
+    Ok(penzd_points)
 }
 
-fn main() {
-   
-    // // If an error occurs print error
-    // if let Err(e) = read_from_stdin() {
-    //     eprintln!("{}", e);
-    // }
-    
+fn main() -> Result<(), Box<dyn Error>> {
+
     let args: Vec<String> = env::args().collect();
     // dbg!(args);
 
     let file_path = &args[1];
-    println!("{:?}", file_path);
-    
-    // If an error occurs print errord
-    if let Err(e) = read_from_file(file_path) {
-        eprintln!("{}", e);
-    }
-    
-}
 
+    // If an error occurs print errord
+    let utm_projections = match read_from_file(file_path) {
+        Ok(penzd_points) => penzd_points,
+        Err(e) => return Err(e),
+    };
+
+    println!("{:?}", utm_projections);
+
+    Ok(())
+}
